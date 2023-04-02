596075784dcbbd6c9e5707a4fc7b2aa126ae6ac8
SLVladislav <bakalavrat.789@gmail.com>
2023-04-02 14:10:52 +0300

    task-img

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Searchbar } from './Searchbar/Searchbar';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import { Container } from './App.styled';
+import { MyGlobalStyles } from 'globalStyles/GlobalStyles.styled';
+
+export class App extends Component {
+  state = {
+    imgTheme: '',
+  };
+
+  handleFormSubmit = imgTheme => {
+    this.setState({ imgTheme });
+  };
+
+  render() {
+    return (
+      <Container>
+        <MyGlobalStyles />
+        <Searchbar onSubmit={this.handleFormSubmit} />
+        <ImageGallery imgTheme={this.state.imgTheme} />
+      </Container>
+    );
+  }
+}
